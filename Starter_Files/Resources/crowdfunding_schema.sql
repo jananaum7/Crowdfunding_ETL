33b0eaf94c8645950d14d107f0e26eab28b9c8c2
monicaphatangare <monica.phatangare@gmail.com>
2024-10-15 20:52:02 -0500

    update

---
+++ Starter_Files/Resources/crowdfunding_schema.sql
@@ -0,0 +1,57 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/zRx4X4
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "contacts" (
+    "contact_id" serial   NOT NULL,
+    "name" varchar(100)   NOT NULL,
+    "first_name" varchar(100)   NOT NULL,
+    "last_name" varchar(100)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(30)   NOT NULL,
+    "category" varchar(30)   NOT NULL,
+    "primary" key(category_id)   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(30)   NOT NULL,
+    "subcategory" varchar(30)   NOT NULL,
+    "primary" key(subcategory_id)   NOT NULL
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" serial   NOT NULL,
+    "company_name" varchar(100)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(100)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(100)   NOT NULL,
+    "currency" varchar(100)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(100)   NOT NULL,
+    "subcategory_id" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
